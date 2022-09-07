ae2f1ffff6fc003a6d0f4209c8d40bf541e00d95
codinglil <102619991+codinglil@users.noreply.github.com>
2022-09-07 12:19:05 -0400

    font change for examples, added antonyms and synonyms h5

--- src/Antonyms.js
+++ src/Antonyms.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 export default function Antonyms(props) {
-  if (props.antonyms) {
+  if (props.antonyms.length !== 0) {
     return (
       <div className="Antonyms">
+        <h5>Antonyms:</h5>
         {props.antonyms.map(function (antonym, index) {
           return (
             <div className="antonym-list" key={index}>
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 export default function Synonyms(props) {
-  if (props.synonyms) {
+  if (props.synonyms.length !== 0) {
     return (
       <div className="Synonyms">
+        <h5>Synonyms:</h5>
         {props.synonyms.map(function (synonym, index) {
           return (
             <div className="synonym-list" key={index}>
