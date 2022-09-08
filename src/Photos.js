23356fe147906866a6aa746b1afc19046dc1f5a0
codinglil <102619991+codinglil@users.noreply.github.com>
2022-09-07 23:18:33 -0400

    added picture source

--- src/Photos.js
+++ src/Photos.js
@@ -9,13 +9,18 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
-                <a href={photo.src.original} target="_blank" rel="referrer">
-                  <img src={photo.src.landscape} className="img-fluid" />
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt="images"
+                  />
                 </a>
               </div>
             );
           })}
         </div>
+        <p className="picture-source">Source: pexels.com</p>
       </section>
     );
   } else {
